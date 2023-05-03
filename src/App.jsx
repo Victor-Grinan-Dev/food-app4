0911f10ecb220675c9175aecb4171f951766b37d
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-05-03 05:29:05 +0300

    fixed positions nav & footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import './App.css';
-import { Routes, Route, HashRouter } from 'react-router-dom';
-import Layout from './pages/Layout';
+import "./App.css";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
 
-import Home from './views/home/Home';
-import About from './views/about/About';
-import NotFound from './views/NotFound/NotFound';
-import Browse from './views/browse/Browse';
-import AddFood from './views/addFood/AddFood';
-import FoodSingle from './views/foodSingle/FoodSingle';
+import Home from "./views/home/Home";
+import About from "./views/about/About";
+import NotFound from "./views/NotFound/NotFound";
+import Browse from "./views/browse/Browse";
+import AddFood from "./views/addFood/AddFood";
+import FoodSingle from "./views/foodSingle/FoodSingle";
 
-console.log('admin@foodapp.dev Abcd1234');
+console.log("admin@foodapp.dev Abcd1234");
 
 function App() {
   return (
-    <HashRouter className="App"> 
-        <Routes>
-              <Route path="/" element={<Layout/>}>
-                <Route index element={<Home/>}></Route>
-                <Route path="about" element={<About/>}></Route> 
-                <Route path="browse" element={<Browse />} />
-                <Route path="add" element={<AddFood />} />
-                <Route path="browse/:single" element={<FoodSingle />} />
-                <Route path="browse/edit/:single" element={<AddFood />} />
-                <Route path="*" element={<NotFound/>}></Route> 
-              </Route>       
-          </Routes>   
+    <HashRouter className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="browse" element={<Browse />} />
+          <Route path="add" element={<AddFood />} />
+          <Route path="browse/:single" element={<FoodSingle />} />
+          <Route path="browse/edit/:single" element={<AddFood />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
     </HashRouter>
   );
 }
--- src/views/browse/Browse.jsx
+++ src/views/browse/Browse.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import axios from 'axios';
-import Card from '../../Ui/Card';
-import css from './browse.module.css';
-import { db } from '../../firebase.js';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import Card from "../../Ui/Card";
+import css from "./browse.module.css";
+import { db } from "../../firebase.js";
 import { collection, getDocs } from "firebase/firestore";
 
 // import {collection, addDoc, Timestamp} from 'firebase/firestore';
 
 // const databaseAPI = 'https://foodapp-c71e2-default-rtdb.europe-west1.firebasedatabase.app/database';
-const countriesAPI = 'https://restcountries.com/v2/all';
+const countriesAPI = "https://restcountries.com/v2/all";
 
 function Browse() {
   const [loading, setLoading] = useState(false);
   const [countries, setCountries] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [recipes, setRecipes] = useState([]);
   const recipesCollectionRef = collection(db, "recipes");
 
   useEffect(() => {
     setLoading(true);
-    const getCountries = () => axios.get(countriesAPI).then(res => {
-      setCountries(res.data);
-    });
+    const getCountries = () =>
+      axios.get(countriesAPI).then((res) => {
+        setCountries(res.data);
+      });
 
     const getRecipes = async () => {
       const data = await getDocs(recipesCollectionRef);
-      setRecipes(data.docs.map(doc=>({...doc.data(), id: doc.id})));
-    }
+      setRecipes(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
     getRecipes();
     getCountries(); //ok
     setLoading(false);
     // eslint-disable-next-line
   }, []);
- 
+
   // const [country, setCountry] = useState([]);
 
-
   const recipesFilter = recipes.filter((res) => {
-    res.name = res.name.toLowerCase()
+    res.name = res.name.toLowerCase();
     return res.name.includes(search.toLowerCase());
   });
 
   const searchHandler = (e) => {
-    setSearch(e.target.value); 
-    };
+    setSearch(e.target.value);
+  };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -52,30 +52,29 @@ function Browse() {
 
   return (
     <div className={css.browse}>
-
       <div className={css.search}>
-         <label> Search </label>
-         <input type="text" className={css.searchImput} placeholder="🔍" onChange={searchHandler} />
-       </div>
-       <div className={css.showCards}>
-         {recipesFilter.map((recipe) => (
-         
-           <Card
-             key={recipe.id}
-             name={recipe.id}
-             data={recipe}
-             country={countries.find(
-               (country) => country.alpha3Code === recipe.country_code
-             )}
-             {...recipe}
-           />
-         ))}
-       </div>
-
+        <input
+          type="text"
+          className={css.searchInput}
+          placeholder="🔍"
+          onChange={searchHandler}
+        />
+      </div>
+      <div className={css.showCards}>
+        {recipesFilter.map((recipe) => (
+          <Card
+            key={recipe.id}
+            name={recipe.id}
+            data={recipe}
+            country={countries.find(
+              (country) => country.alpha3Code === recipe.country_code
+            )}
+            {...recipe}
+          />
+        ))}
+      </div>
     </div>
   );
-};
+}
 
 export default Browse;
-
-
